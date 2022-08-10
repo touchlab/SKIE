841c1709c83738de1dc416f4a459d7d471bf350a
Tadeas Kriz <tadeas@touchlab.co>
2022-08-10 16:08:41 -0400

    Fix crash when no Swift files are produced.

--- swiftkt-plugin/kotlin-plugin/src/main/kotlin/co/touchlab/swiftkt/plugin/SwiftKtCompilePhase.kt
+++ swiftkt-plugin/kotlin-plugin/src/main/kotlin/co/touchlab/swiftkt/plugin/SwiftKtCompilePhase.kt
@@ -1,13 +1,7 @@
 package co.touchlab.swiftkt.plugin
 
-import co.touchlab.swiftpack.spec.KobjcTransforms
-import co.touchlab.swiftpack.spec.KotlinFileReference
-import co.touchlab.swiftpack.spec.KotlinFunctionReference
-import co.touchlab.swiftpack.spec.KotlinPropertyReference
-import co.touchlab.swiftpack.spec.KotlinTypeReference
 import co.touchlab.swiftpack.spec.SwiftPackModule
 import co.touchlab.swiftpack.spi.NamespacedSwiftPackModule
-import co.touchlab.swiftpack.spi.SwiftNameProvider
 import co.touchlab.swiftpack.spi.produceSwiftFile
 import org.jetbrains.kotlin.backend.common.CommonBackendContext
 import org.jetbrains.kotlin.backend.konan.BitcodeEmbedding
@@ -21,58 +15,6 @@ import org.jetbrains.kotlin.konan.target.platformName
 import org.jetbrains.kotlin.konan.target.withOSVersion
 import org.jetbrains.kotlin.library.impl.javaFile
 import java.io.File
-
-fun List<KobjcTransforms>.merge(): KobjcTransforms {
-    val types = this.flatMap { it.types.entries }.groupBy({ it.key }, { it.value }).mapValues { it.value.merge(it.key) }
-    val files = this.flatMap { it.files.entries }.groupBy({ it.key }, { it.value }).mapValues { it.value.merge(it.key) }
-    val properties = this.flatMap { it.properties.entries }.groupBy({ it.key }, { it.value }).mapValues { it.value.merge(it.key) }
-    val functions = this.flatMap { it.functions.entries }.groupBy({ it.key }, { it.value }).mapValues { it.value.merge(it.key) }
-
-    return KobjcTransforms(types, files, properties, functions)
-}
-
-fun List<KobjcTransforms.TypeTransform>.merge(reference: KotlinTypeReference): KobjcTransforms.TypeTransform {
-    val properties = this.flatMap { it.properties.entries }.groupBy({ it.key }, { it.value }).mapValues { it.value.merge(it.key) }
-    val functions = this.flatMap { it.methods.entries }.groupBy({ it.key }, { it.value }).mapValues { it.value.merge(it.key) }
-
-    return KobjcTransforms.TypeTransform(
-        reference = reference,
-        hide = any { it.hide },
-        remove = any { it.remove },
-        rename = mapNotNull { it.rename }.singleOrNull(),
-        bridge = mapNotNull { it.bridge }.singleOrNull(),
-        properties = properties,
-        methods = functions,
-    )
-}
-
-fun List<KobjcTransforms.FileTransform>.merge(reference: KotlinFileReference): KobjcTransforms.FileTransform {
-    return KobjcTransforms.FileTransform(
-        reference = reference,
-        hide = any { it.hide },
-        remove = any { it.remove },
-        rename = mapNotNull { it.rename }.singleOrNull(),
-        bridge = mapNotNull { it.bridge }.singleOrNull(),
-    )
-}
-
-fun List<KobjcTransforms.PropertyTransform>.merge(reference: KotlinPropertyReference): KobjcTransforms.PropertyTransform {
-    return KobjcTransforms.PropertyTransform(
-        reference = reference,
-        hide = any { it.hide },
-        remove = any { it.remove },
-        rename = mapNotNull { it.rename }.singleOrNull(),
-    )
-}
-
-fun List<KobjcTransforms.FunctionTransform>.merge(reference: KotlinFunctionReference): KobjcTransforms.FunctionTransform {
-    return KobjcTransforms.FunctionTransform(
-        reference = reference,
-        hide = any { it.hide },
-        remove = any { it.remove },
-        rename = mapNotNull { it.rename }.singleOrNull(),
-    )
-}
 
 class SwiftKtCompilePhase(
     val swiftPackModuleReferences: List<NamespacedSwiftPackModule.Reference>,
@@ -133,6 +75,11 @@ class SwiftKtCompilePhase(
         val modulemapFile = framework.resolve("Modules/module.modulemap")
         val apiNotes = ApiNotes(moduleName, transformResolver)
         apiNotes.save(headersDir)
+
+        // We want to make sure we generate .apinotes file even if there are no Swift source files.
+        if (sourceFiles.isEmpty()) {
+            return emptyList()
+        }
 
         val swiftObjectsDir = config.tempFiles.create("swift-object").also { it.mkdirs() }
 
