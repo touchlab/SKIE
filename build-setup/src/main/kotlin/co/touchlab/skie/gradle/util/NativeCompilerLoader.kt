a77ea6ff68aa313232d60dd44393a0db92a5483a
Filip Dolník <filip@brightify.org>
2022-10-31 13:17:18 +0100

    Fix kotlin native compiler download script.

--- build-setup/src/main/kotlin/co/touchlab/skie/gradle/util/NativeCompilerLoader.kt
+++ build-setup/src/main/kotlin/co/touchlab/skie/gradle/util/NativeCompilerLoader.kt
@@ -3,12 +3,13 @@ package co.touchlab.skie.gradle.util
 import org.gradle.api.Project
 import org.gradle.api.file.FileCollection
 import org.jetbrains.kotlin.gradle.utils.NativeCompilerDownloader
+import java.io.File
 
 fun Project.extractedKotlinNativeCompilerEmbeddable(): FileCollection {
     val targetFile = layout.buildDirectory.file("tmp/kotlin-native").map {
         val file = it.asFile
         if (!file.exists()) {
-            val tree = zipTree(kotlinNativeCompilerEmbeddable())
+            val tree = zipTree(kotlinNativeCompilerEmbeddableFile())
 
             copy {
                 from(tree)
@@ -23,8 +24,10 @@ fun Project.extractedKotlinNativeCompilerEmbeddable(): FileCollection {
 }
 
 fun Project.kotlinNativeCompilerEmbeddable(): FileCollection =
+    files(kotlinNativeCompilerEmbeddableFile())
+
+private fun Project.kotlinNativeCompilerEmbeddableFile(): File =
     NativeCompilerDownloader(project)
         .also { it.downloadIfNeeded() }
         .compilerDirectory
         .resolve("konan/lib/kotlin-native-compiler-embeddable.jar")
-        .let { files(it) }
