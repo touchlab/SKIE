82ba3bcc6adf87a400684a1575ad0a27e06566dd
Filip Dolník <filip@brightify.org>
2022-11-01 11:02:24 +0100

    Fix JVM artifact publishing.

--- build-setup/src/main/kotlin/co/touchlab/skie/gradle/publish/BaseJavaSkiePublishPlugin.kt
+++ build-setup/src/main/kotlin/co/touchlab/skie/gradle/publish/BaseJavaSkiePublishPlugin.kt
@@ -2,14 +2,14 @@ package co.touchlab.skie.gradle.publish
 
 import org.gradle.api.Project
 import org.gradle.api.plugins.JavaPluginExtension
-import org.jetbrains.kotlin.gradle.plugin.KotlinPlatformJvmPlugin
+import org.jetbrains.kotlin.gradle.plugin.KotlinPluginWrapper
 
 abstract class BaseJavaSkiePublishPlugin : BaseSkiePublishPlugin() {
 
     override fun apply(target: Project) {
         super.apply(target)
 
-        target.plugins.withType(KotlinPlatformJvmPlugin::class.java) {
+        target.plugins.withType(KotlinPluginWrapper::class.java) {
             afterKotlinPlugin(target)
         }
     }
