924a695559ee5d90c9a3f16acc0a6894444293e3
Filip Dolník <filip@brightify.org>
2022-11-02 10:04:28 +0100

    Fix Gradle plugin publication.

--- build-setup/src/main/kotlin/co/touchlab/skie/gradle/publish/SkiePublishGradlePlugin.kt
+++ build-setup/src/main/kotlin/co/touchlab/skie/gradle/publish/SkiePublishGradlePlugin.kt
@@ -2,29 +2,70 @@ package co.touchlab.skie.gradle.publish
 
 import com.gradle.publish.PublishPlugin
 import org.gradle.api.Project
+import org.gradle.api.publish.Publication
+import org.gradle.api.publish.PublicationContainer
 import org.gradle.api.publish.PublishingExtension
 import org.gradle.api.publish.maven.MavenPublication
+import org.w3c.dom.Element
+import org.w3c.dom.NodeList
 
 class SkiePublishGradlePlugin : BaseJavaSkiePublishPlugin() {
 
     override fun apply(target: Project) {
         super.apply(target)
 
-        applyGradlePublishingPlugin(target)
-        configurePublication(target)
+        with(target) {
+            applyGradlePublishingPlugin()
+            configurePublication()
+        }
     }
 
-    private fun applyGradlePublishingPlugin(target: Project) {
-        target.plugins.apply(PublishPlugin::class.java)
+    private fun Project.applyGradlePublishingPlugin() {
+        plugins.apply(PublishPlugin::class.java)
     }
 
-    private fun configurePublication(target: Project) {
-        target.afterEvaluate {
-            target.extensions.configure(PublishingExtension::class.java) {
-                val publication = publications.getByName("pluginMaven") as MavenPublication
-
-                publication.artifactId = target.mavenArtifactId
+    private fun Project.configurePublication() {
+        afterEvaluate {
+            extensions.configure(PublishingExtension::class.java) {
+                configureMainPublicationName(publications)
             }
+        }
+    }
+
+    private fun Project.configureMainPublicationName(publications: PublicationContainer) {
+        val mainPublication = publications.first { it.isMainPublication }
+        val markerPublications = publications.filterNot { it.isMainPublication }
+
+        renameMainPublication(mainPublication)
+        markerPublications.forEach {
+            fixDependencyInMarkerPublication(it)
+        }
+    }
+
+    private val Publication.isMainPublication: Boolean
+        get() = this.name == "pluginMaven"
+
+    private fun Project.renameMainPublication(publication: Publication) {
+        require(publication is MavenPublication) { "Only Maven publications are supported." }
+
+        publication.artifactId = mavenArtifactId
+    }
+
+    private fun Project.fixDependencyInMarkerPublication(publication: Publication) {
+        require(publication is MavenPublication) { "Only Maven publications are supported." }
+
+        publication.pom.withXml {
+            this.asElement().getElementsByTagName("dependencies").forEach { dependencies ->
+                dependencies.getElementsByTagName("artifactId").forEach { artifactId ->
+                    artifactId.textContent = mavenArtifactId
+                }
+            }
+        }
+    }
+
+    private inline fun NodeList.forEach(action: (Element) -> Unit) {
+        for (i in 0 until this.length) {
+            action(this.item(i) as Element)
         }
     }
 }
