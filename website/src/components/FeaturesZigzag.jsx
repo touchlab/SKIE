d62de562ebe25de044f7c7df783550d1d3716135
Kevin Galligan <kgalligan@gmail.com>
2022-12-14 22:16:12 -0500

    Update landing page

--- website/src/components/FeaturesZigzag.jsx
+++ website/src/components/FeaturesZigzag.jsx
@@ -8,9 +8,10 @@ import FeatImage03 from '@site/static/componentimg/features-03-image-03.png';
 import enumKotlin from '@site/static/samples/enum/kotlin.png';
 import enumSwiftBefore from '@site/static/samples/enum/swiftbefore.png';
 import enumSwiftAfter from '@site/static/samples/enum/swiftafter.png';
-import sealedKotlin from '@site/static/samples/sealed-class/kotlin-sealed-class.png';
-import sealedSwiftBefore from '@site/static/samples/sealed-class/swift-before-skie-sealed-class.png';
-import sealedSwiftAfter from '@site/static/samples/sealed-class/swift-after-skie-sealed-class.png';
+import sealedKotlin from '@site/static/samples/sealed-class/kotlin.png';
+import sealedSwiftBefore from '@site/static/samples/sealed-class/swiftbefore.png';
+import sealedSwiftAfter from '@site/static/samples/sealed-class/swiftafter.png';
+import sealedSwiftAfterComplete from '@site/static/samples/sealed-class/swiftaftercomplete.png';
 
 function ShowKotlinSwiftToggle(kotlinBlock, swiftBlock){
     const [showKotlin, setShowKotlin] = useState(true);
@@ -38,45 +39,100 @@ function ShowKotlinSwiftToggle(kotlinBlock, swiftBlock){
 function labelImage(label, imgObj) {
     return (
         <>
-            <div
-                className="w-48 mt-4 mb-2 text-center inline-block text-black bg-slate-400 dark:text-white dark:bg-slate-600 py-1 px-3 rounded-full text-lg">{label}</div>
-            <br/>
-
-            <img className=" " src={imgObj}
+            <img src={imgObj}
                  alt="Features 01"/><br/>
         </>
     )
 }
 
-function ShowKotlinSwiftFriends(imgK, imgS, imgSAfter) {
+function ToggleBlock(currentBlock, blockName, setBlock, title, svgBody){
+    return(
+        <div
+            className={`flex flex-row justify-center items-center font-medium py-2 px-4 m-2 bg-gray-100 dark:bg-gray-800 rounded-full group ${currentBlock !== blockName && 'opacity-50 hover:opacity-75 cursor-pointer'}`}
+            onClick={() => { setBlock(blockName);}}
+        >
+            <span className="mr-2 p-0 -mb-1">{svgBody()}</span>
+            <span className="text-gray-600 group-hover:text-gray-800 dark:text-gray-400 dark:group-hover:text-gray-200 transition-colors duration-150 ease-in-out">{title}</span>
+        </div>
+    )
+}
+
+function ShowKotlinSwiftFriends(imgK, imgS, imgSAfter, imgSAfterComplete) {
+    const [block, setBlock] = useState("kotlin");
+
   return (
       <>
-        {/*<div className="block md:hidden items-center">
-          {ShowKotlinSwiftToggle(() => {
-            return (
-                <img className="max-w-full mx-auto md:max-w-none h-auto" src={imgK} width="540" height="auto"
-                     alt="Features 01"/>
-            )
-          }, () => {
-            return (
-                <img className="max-w-full mx-auto md:max-w-none h-auto" src={imgS} width="540" height="auto"
-                     alt="Features 01"/>
-            )
-          })}
-        </div>*/}
+      <div className="max-w-6xl mx-auto px-4 sm:px-6">
+          <div className="flex flex-wrap justify-center -m-2 my-8">
+              {ToggleBlock(
+                  block,
+                  "kotlin",
+                  setBlock,
+                  "Kotlin",
+                  AndroidOnly
+              )}
+              {ToggleBlock(
+                  block,
+                  "noskie",
+                  setBlock,
+                  "Without SKIE",
+                  AppleOnly
+              )}
+              {ToggleBlock(
+                  block,
+                  "withskie",
+                  setBlock,
+                  "With SKIE",
+                  DecisionProcessOnly
+              )}
+              {imgSAfterComplete && ToggleBlock(
+                  block,
+                  "withskiecomplete",
+                  setBlock,
+                  "With SKIE Fixed",
+                  DecisionProcessOnly
+              )}
 
-      <div className="max-w-4xl mx-auto text-center">
+          </div>
+      </div>
+      <div className="relative max-w-4xl mx-auto text-center">
+          <img className="absolute invisible" src={imgK}
+               alt="Features 01"/>
           {/* Image */}
+          {block === "kotlin" &&
+              <>
             {labelImage("Kotlin", imgK)}
-            {labelImage("Without SKIE ❌", imgS)}
-            {labelImage("With SKIE ✅", imgSAfter)}
+              </>
+          }
+          {block === "noskie" &&
+              <>
+                  {labelImage("Without SKIE ❌", imgS)}
+              </>
+          }
+          {block === "withskie" &&
+              <>
+                  {labelImage("With SKIE ✅", imgSAfter)}
+              </>
+          }
+          {imgSAfterComplete && block === "withskiecomplete" &&
+              <>
+                  {labelImage("With SKIE ✅", imgSAfterComplete)}
+              </>
+          }
+
         </div>
       </>
   )
 }
 
-function FeaturesZigzag() {
-
+export default function FeaturesZigzag() {
+    const checkMark = (color) => {
+        return (
+            <svg className={`w-3 h-3 fill-current text-${color}-500 mr-2 shrink-0" viewBox="0 0 12 12`} xmlns="http://www.w3.org/2000/svg">
+                <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+            </svg>
+        )
+    }
     return (
     <section>
       <div className="hidden text-lime-600"></div>
@@ -97,15 +153,11 @@ function FeaturesZigzag() {
               <p className="text-xl text-gray-700 dark:text-gray-400 mb-4">Kotlin enums are automatically and transparently converted to Swift enums.</p>
               <ul className="text-lg text-gray-700 dark:text-gray-400 -mb-2">
                 <li className="flex items-center mb-2">
-                  <svg className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
-                  </svg>
+                    {checkMark("green")}
                   <span>Proper compile-time checking</span>
                 </li>
                 <li className="flex items-center mb-2">
-                  <svg className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
-                  </svg>
+                    {checkMark("green")}
                   <span>Swift-native ergonomics</span>
                 </li>
               </ul>
@@ -119,8 +171,37 @@ function FeaturesZigzag() {
 
         </div>
       </div>
+        <div className="max-w-4xl mx-auto px-4 sm:px-6">
+            <div className="pb-12 md:pb-20 border-t border-gray-800">
+
+                {/* Items */}
+                <div className="">
+
+                    <div className="items-center">
+                        <div className="font-architects-daughter text-xl text-lime-700 dark:text-lime-300 mb-2">Sealed class support</div>
+                        <h3 className="h3 mb-3">Sealed class wrapped as an enum</h3>
+                        <p className="text-xl text-gray-700 dark:text-gray-400 mb-4">Sealed classes are unchanged, but an associated enum is
+                        generated, as well as a wrapper function to use for switch statements.</p>
+                        <ul className="text-lg text-gray-700 dark:text-gray-400 -mb-2">
+                            <li className="flex items-center mb-2">
+                                {checkMark("green")}
+                                <span>Sealed classes can be exhaustively checked</span>
+                            </li>
+                            <li className="flex items-center mb-2">
+                                {checkMark("green")}
+                                <span>Similar semantics to enums with associated values</span>
+                            </li>
+                        </ul>
+                    </div>
+
+                    {/* 1st item */}
+
+                    {ShowKotlinSwiftFriends(sealedKotlin, sealedSwiftBefore, sealedSwiftAfter, sealedSwiftAfterComplete)}
+
+                </div>
+
+            </div>
+        </div>
     </section>
   );
 }
-
-export default FeaturesZigzag;
