014405fbdc1e9c45267e8ad0e8491f6cb032155a
Filip Dolník <filip@brightify.org>
2022-12-19 12:05:33 +0100

    Change SwiftRuntime feature flag to not affect type alias generation.

--- plugin/generator/src/main/kotlin/co/touchlab/skie/plugin/generator/internal/typealias/TypeAliasGenerator.kt
+++ plugin/generator/src/main/kotlin/co/touchlab/skie/plugin/generator/internal/typealias/TypeAliasGenerator.kt
@@ -20,7 +20,7 @@ internal class TypeAliasGenerator(
     configuration: Configuration,
 ) : SkieCompilationPhase {
 
-    override val isActive: Boolean = SkieFeature.SwiftRuntime in configuration.enabledFeatures
+    override val isActive: Boolean = true
 
     private val areTypeAliasesExported = SkieFeature.FqNames in configuration.enabledFeatures
 
