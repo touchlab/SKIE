2c4402d59efb8af1f1404f164f761dcdb8f83000
Filip Dolník <filip@brightify.org>
2023-02-01 16:01:24 +0100

    Make suspend wrappers enabled by default.

--- plugin/gradle-plugin/src/main/java/co/touchlab/skie/plugin/SkieFeatureConfiguration.kt
+++ plugin/gradle-plugin/src/main/java/co/touchlab/skie/plugin/SkieFeatureConfiguration.kt
@@ -8,7 +8,7 @@ import javax.inject.Inject
 
 abstract class SkieFeatureConfiguration @Inject constructor(objects: ObjectFactory) {
 
-    val suspendInterop: Property<Boolean> = objects.property(Boolean::class.java).convention(false)
+    val suspendInterop: Property<Boolean> = objects.property(Boolean::class.java).convention(true)
     val fqNames: Property<Boolean> = objects.property(Boolean::class.java).convention(false)
     val swiftRuntime: Property<Boolean> = objects.property(Boolean::class.java).convention(true)
 
