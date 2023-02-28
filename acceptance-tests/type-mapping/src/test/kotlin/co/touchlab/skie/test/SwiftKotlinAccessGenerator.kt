0664d267dfda40a7201369526df95e242f0891a2
Tadeas Kriz <tadeas@touchlab.co>
2023-02-27 19:08:36 -0500

    Update type mapping test to verify the exact type is used.

--- acceptance-tests/type-mapping/src/test/kotlin/co/touchlab/skie/test/SwiftKotlinAccessGenerator.kt
+++ acceptance-tests/type-mapping/src/test/kotlin/co/touchlab/skie/test/SwiftKotlinAccessGenerator.kt
@@ -10,8 +10,11 @@ import co.touchlab.skie.plugin.api.model.type.stableSpec
 import co.touchlab.skie.plugin.api.skieContext
 import co.touchlab.skie.plugin.intercept.PhaseListener
 import io.outfoxx.swiftpoet.ANY_OBJECT
+import io.outfoxx.swiftpoet.AttributeSpec
 import io.outfoxx.swiftpoet.CodeBlock
+import io.outfoxx.swiftpoet.DeclaredTypeName
 import io.outfoxx.swiftpoet.FunctionSpec
+import io.outfoxx.swiftpoet.FunctionTypeName
 import io.outfoxx.swiftpoet.ParameterSpec
 import io.outfoxx.swiftpoet.PropertySpec
 import io.outfoxx.swiftpoet.TypeSpec
@@ -33,6 +36,100 @@ internal class SwiftKotlinAccessGenerator: PhaseListener {
 
         val kotlinClass = descriptorProvider.exposedClasses.first {
             it.name.identifier == "KotlinFile"
+        }
+
+        context.skieContext.module.file(
+            "TypeVerification",
+            """
+                @resultBuilder
+                struct VerifyReturnType<ReturnType> {
+                    let value: ReturnType
+
+                    static func buildBlock<T>(_ components: T...) -> VerifyReturnType<T> {
+                        return VerifyReturnType<T>(value: components[0])
+                    }
+
+                    static func buildFinalResult(_ component: VerifyReturnType<ReturnType>) -> ReturnType {
+                        return component.value
+                    }
+                }
+
+                @propertyWrapper
+                struct VerifyParamType<Final> {
+                    let wrappedValue: Final
+
+                    var projectedValue: VerifyParamType<Final> {
+                        self
+                    }
+
+                    init(wrappedValue: Final) {
+                        self.wrappedValue = wrappedValue
+                    }
+
+                    init(projectedValue: VerifyParamType<Final>) {
+                        self.wrappedValue = projectedValue.wrappedValue
+                    }
+                }
+            """.trimIndent())
+
+        context.skieContext.module.file("TypeVerification+verify") {
+            val parameterCounts = descriptorProvider.getAllExposedMembers(kotlinClass)
+                .filter {
+                    !DescriptorUtils.isMethodOfAny(it)
+                }
+                .flatMap {
+                    it.swiftModel.directlyCallableMembers
+                }
+                .filterIsInstance<KotlinFunctionSwiftModel>()
+                .map { it.valueParameters.size }
+                .toSet()
+
+            /**
+             * func verify<P1, T>(function: @escaping (P1) -> T) -> (VerifyParamType<P1>) -> T {
+             *     return { $p1 in
+             *         function(p1)
+             *     }
+             * }
+             */
+            parameterCounts.filter { it >= 1 }.forEach { paramCount ->
+                val paramTypeVariables = (1..paramCount).map {
+                    TypeVariableName.typeVariable("P$it")
+                }
+                val returnTypeVariable = TypeVariableName.typeVariable("T")
+
+                addFunction(
+                    FunctionSpec.builder("verify")
+                        .addTypeVariables(paramTypeVariables)
+                        .addTypeVariable(returnTypeVariable)
+                        .addParameter(
+                            "function", FunctionTypeName.get(
+                                parameters = paramTypeVariables.map {
+                                    ParameterSpec.unnamed(it)
+                                },
+                                returnType = returnTypeVariable,
+                                attributes = listOf(AttributeSpec.ESCAPING),
+                            ),
+                        )
+                        .returns(
+                            FunctionTypeName.get(
+                                parameters = paramTypeVariables.map {
+                                    ParameterSpec.unnamed(DeclaredTypeName.qualifiedTypeName(".VerifyParamType").parameterizedBy(it))
+                                },
+                                returnType = returnTypeVariable,
+                            )
+                        )
+                        .addCode(
+                            """
+                                return { %L in
+                                    function(%L)
+                                }
+                            """.trimIndent(),
+                            paramTypeVariables.indices.map { CodeBlock.of("${"$"}p$it") }.joinToCode(),
+                            paramTypeVariables.indices.map { CodeBlock.of("p$it") }.joinToCode(),
+                        )
+                        .build()
+                )
+            }
         }
 
         context.skieContext.module.file("KotlinFile_access") {
@@ -70,6 +167,7 @@ internal class SwiftKotlinAccessGenerator: PhaseListener {
                                     is KotlinRegularPropertySwiftModel -> {
                                         addProperty(
                                             PropertySpec.builder(swiftModel.identifier, swiftModel.type.stableSpec)
+                                                .addAttribute(CodeBlock.of("VerifyReturnType<%T>", swiftModel.type.stableSpec).toString())
                                                 .getter(
                                                     FunctionSpec.getterBuilder()
                                                         .addStatement("kotlinClass.%L", swiftModel.reference)
@@ -84,21 +182,28 @@ internal class SwiftKotlinAccessGenerator: PhaseListener {
                                         }
                                         addFunction(
                                             FunctionSpec.builder(swiftModel.identifier)
+                                                .addAttribute(CodeBlock.of("VerifyReturnType<%T>", swiftModel.returnType.stableSpec).toString())
                                                 .addParameters(
                                                     swiftModel.valueParameters.map { parameter ->
-                                                        ParameterSpec.builder(
-                                                            parameter.argumentLabel,
-                                                            parameter.parameterName,
-                                                            parameter.type.stableSpec,
-                                                        ).build()
+                                                        ParameterSpec
+                                                            .builder(
+                                                                parameter.argumentLabel,
+                                                                parameter.parameterName,
+                                                                parameter.type.stableSpec,
+                                                            )
+                                                            .addAttribute("VerifyParamType")
+                                                            .build()
                                                     }
                                                 )
                                                 .returns(swiftModel.returnType.stableSpec)
                                                 .addCode(
-                                                    "kotlinClass.%N(%L)",
+                                                    """
+                                                        let functionVerification = verify(function: kotlinClass.%N)
+                                                        functionVerification(%L)
+                                                    """.trimIndent(),
                                                     swiftModel.reference,
                                                     swiftModel.valueParameters.map { parameter ->
-                                                        CodeBlock.of("%N", parameter.parameterName)
+                                                        CodeBlock.of("_%N", parameter.parameterName)
                                                     }.joinToCode(),
                                                 )
                                                 .build()
