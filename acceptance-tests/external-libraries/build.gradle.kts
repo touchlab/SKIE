87d09de44de564f74c10aca02a7b68d59aa0f49d
Tadeas Kriz <tadeas@touchlab.co>
2023-03-05 22:19:08 -0500

    Add better stats and CSV exports.
    [skip ci]

--- acceptance-tests/external-libraries/build.gradle.kts
+++ acceptance-tests/external-libraries/build.gradle.kts
@@ -182,6 +182,7 @@ tasks.register<Test>("comparePureAndSkie") {
 
     systemProperty("pureTestDir", layout.buildDirectory.dir("external-libraries-tests-no-skie").get().asFile.absolutePath)
     systemProperty("skieTestDir", layout.buildDirectory.dir("external-libraries-tests").get().asFile.absolutePath)
+    systemProperty("csvOutput", layout.buildDirectory.file("reports/comparison-report.csv").get().asFile.absolutePath)
 
     description = "Compares library tests with and without SKIE"
     group = "verification"
