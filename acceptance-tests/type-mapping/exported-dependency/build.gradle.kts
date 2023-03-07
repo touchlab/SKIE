8042c6f96c276d4d10b9f7b8d094760141d5ea30
Tadeas Kriz <tadeas@touchlab.co>
2023-03-07 13:42:49 -0500

    Add ios targets to type mapping test dependencies.

--- acceptance-tests/type-mapping/exported-dependency/build.gradle.kts
+++ acceptance-tests/type-mapping/exported-dependency/build.gradle.kts
@@ -5,6 +5,8 @@ plugins {
 kotlin {
     macosX64()
     macosArm64()
+    ios()
+    iosSimulatorArm64()
 
     val commonMain by sourceSets.getting {
         dependencies {
@@ -22,5 +24,15 @@ kotlin {
 
     val macosX64Main by sourceSets.getting {
         dependsOn(macosMain)
+    }
+
+    val iosMain by sourceSets.getting
+    val iosTest by sourceSets.getting
+
+    val iosSimulatorArm64Main by sourceSets.getting {
+        dependsOn(iosMain)
+    }
+    val iosSimulatorArm64Test by sourceSets.getting {
+        dependsOn(iosTest)
     }
 }
