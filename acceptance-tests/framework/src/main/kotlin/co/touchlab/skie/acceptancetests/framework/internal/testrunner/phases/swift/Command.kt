7e7228d2857385806eb8cd8c7099380aca250da7
Tadeas Kriz <tadeas@touchlab.co>
2023-03-12 14:40:18 -0400

    Fix command execution to enforce Swift binary timeouts and reenable acceptance tests.

--- acceptance-tests/framework/src/main/kotlin/co/touchlab/skie/acceptancetests/framework/internal/testrunner/phases/swift/Command.kt
+++ acceptance-tests/framework/src/main/kotlin/co/touchlab/skie/acceptancetests/framework/internal/testrunner/phases/swift/Command.kt
@@ -13,10 +13,27 @@ internal fun String.execute(): CommandResult {
         .redirectErrorStream(true)
         .start()
 
-    process.waitFor(1, TimeUnit.MINUTES)
-
-    return CommandResult(
-        exitCode = process.exitValue(),
-        stdOut = process.inputStream.bufferedReader().readText(),
-    )
+    val hasExited = process.waitFor(1, TimeUnit.MINUTES)
+    if (hasExited) {
+        return CommandResult(
+            exitCode = process.exitValue(),
+            stdOut = process.inputStream.bufferedReader().readText(),
+        )
+    } else {
+        val commandLine = process.info().commandLine().orElse("N/A")
+        println("Process `${commandLine}` has not exited after 1 minute. Sending SIGTERM signal.")
+        process.destroy()
+        val hasBeenTerminated = process.waitFor(1, TimeUnit.MINUTES)
+        if (hasBeenTerminated) {
+            throw IllegalStateException("Process `${commandLine}` has been terminated after timing out!")
+        } else {
+            println("Process `${commandLine}` has not been terminated after 1 minute. Sending SIGKILL signal.")
+            val hasBeenKilled = process.destroyForcibly().waitFor(5, TimeUnit.MINUTES)
+            if (hasBeenKilled) {
+                throw IllegalStateException("Process `${commandLine}` has been killed after termination timed out!")
+            } else {
+                throw UnknownError("Unable to terminate process `${commandLine}`! Restart of the machine is advised!")
+            }
+        }
+    }
 }
