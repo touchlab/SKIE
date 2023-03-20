322f69f152fdfcb5a7f9da414abe66371650527f
Tadeas Kriz <tadeas@touchlab.co>
2023-03-20 12:00:18 -0400

    Change SKIE phasing so it aligns with new Kotlin phases.

--- plugin/api/src/main/kotlin/co/touchlab/skie/plugin/api/kotlin/MutableDescriptorProvider.kt
+++ plugin/api/src/main/kotlin/co/touchlab/skie/plugin/api/kotlin/MutableDescriptorProvider.kt
@@ -3,12 +3,6 @@ package co.touchlab.skie.plugin.api.kotlin
 interface MutableDescriptorProvider: DescriptorProvider {
     fun mutate(block: DescriptorRegistrationScope.() -> Unit)
 
-    /*
-     * Disables registration of new descriptors. This has to be called before any descriptors are
-     * used for further computations that may be cached.
-     */
-    fun preventFurtherMutations(): DescriptorProvider
-
     /**
      * Register a listener that will be called each time the provider is mutated.
      */
