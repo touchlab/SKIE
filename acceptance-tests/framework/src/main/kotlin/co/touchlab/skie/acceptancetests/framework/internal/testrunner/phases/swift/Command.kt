95d0b557266e191d3743e8d7cb63caa030b29df8
Tadeas Kriz <tadeas@touchlab.co>
2023-03-22 09:02:37 -0400

    Fix String.execute to not hang the tests.

--- acceptance-tests/framework/src/main/kotlin/co/touchlab/skie/acceptancetests/framework/internal/testrunner/phases/swift/Command.kt
+++ acceptance-tests/framework/src/main/kotlin/co/touchlab/skie/acceptancetests/framework/internal/testrunner/phases/swift/Command.kt
@@ -1,39 +1,43 @@
 package co.touchlab.skie.acceptancetests.framework.internal.testrunner.phases.swift
 
+import org.jetbrains.kotlin.konan.file.createTempFile
+import org.jetbrains.kotlin.library.impl.javaFile
+
 import java.util.concurrent.TimeUnit
+import java.util.logging.Logger
 
-internal data class CommandResult(val exitCode: Int, val stdOut: String)
+internal class CommandResult(val exitCode: Int, val stdOut: String)
 
-internal fun String.execute(): CommandResult {
+internal fun String.execute(logger: Logger = Logger.getLogger("String.execute")): CommandResult {
     val command = this.split("\\s".toRegex())
 
+    val tempOutputFile = createTempFile("skie-test-runner", ".out").also { it.deleteOnExit() }
     val process = ProcessBuilder(*command.toTypedArray())
-        .redirectOutput(ProcessBuilder.Redirect.PIPE)
-        .redirectError(ProcessBuilder.Redirect.PIPE)
+        .redirectOutput(ProcessBuilder.Redirect.to(tempOutputFile.javaFile()))
         .redirectErrorStream(true)
         .start()
 
     val hasExited = process.waitFor(1, TimeUnit.MINUTES)
-    if (hasExited) {
-        return CommandResult(
-            exitCode = process.exitValue(),
-            stdOut = process.inputStream.bufferedReader().readText(),
-        )
-    } else {
+    if (!hasExited) {
         val commandLine = process.info().commandLine().orElse("N/A")
-        println("Process `${commandLine}` has not exited after 1 minute. Sending SIGTERM signal.")
+        logger.warning("Process `${commandLine}` has not exited after 1 minute. Sending SIGTERM signal.")
         process.destroy()
         val hasBeenTerminated = process.waitFor(1, TimeUnit.MINUTES)
         if (hasBeenTerminated) {
-            throw IllegalStateException("Process `${commandLine}` has been terminated after timing out!")
+            logger.warning("Process `${commandLine}` has been terminated after timing out!")
         } else {
-            println("Process `${commandLine}` has not been terminated after 1 minute. Sending SIGKILL signal.")
+            logger.warning("Process `${commandLine}` has not been terminated after 1 minute. Sending SIGKILL signal.")
             val hasBeenKilled = process.destroyForcibly().waitFor(5, TimeUnit.MINUTES)
             if (hasBeenKilled) {
-                throw IllegalStateException("Process `${commandLine}` has been killed after termination timed out!")
+                logger.warning("Process `${commandLine}` has been killed after termination timed out!")
             } else {
-                throw UnknownError("Unable to terminate process `${commandLine}`! Restart of the machine is advised!")
+                logger.severe("Unable to terminate process `${commandLine}`! Restart of the machine is advised!")
             }
         }
     }
+
+    return CommandResult(
+        exitCode = process.exitValue(),
+        stdOut = tempOutputFile.javaFile().readText(),
+    )
 }
