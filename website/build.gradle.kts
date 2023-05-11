d784b220c5b26c262ff0e01074d1aa1637051e39
Filip Dolník <filip@brightify.org>
2023-05-11 11:42:37 +0200

    Fix SKIE version replacement in docs.

--- website/build.gradle.kts
+++ website/build.gradle.kts
@@ -1,3 +1,5 @@
+import co.touchlab.touchlabtools.DocusaurusOssTemplateExtension
+
 plugins {
     alias(libs.plugins.docusaurusOssTemplate)
 }
@@ -12,4 +14,43 @@ tasks.register("cleanAll") {
 
 docusaurusOss {
     destination.set(projectDir)
+}
+
+task("replaceSkieVersions") {
+    group = "DocusaurusOssTemplate"
+
+    doLast {
+        val directory = getDocusaurusDirectory()
+
+        val baseSkieVersion = (project.property("LATEST_GITHUB_VERSION") as String).split("-")[0];
+
+        val replacements = listOf(
+            "{{SKIE_VERSION}}" to baseSkieVersion,
+            "{{SKIE_VERSION_PREVIEW}}" to "$baseSkieVersion-preview.1.8.20",
+        )
+
+        directory.recursiveReplace(replacements)
+    }
+}
+
+fun getDocusaurusDirectory(): File {
+    val extension = extensions.getByType<DocusaurusOssTemplateExtension>()
+
+    val destinationDir = extension.destination.get()
+
+    return File(destinationDir, "docs")
+}
+
+fun File.recursiveReplace(replacements: List<Pair<String, String>>) {
+    walkTopDown()
+        .filter { it.isFile && it.extension in listOf("md", "mdx") }
+        .forEach { file ->
+            val text = file.readText()
+
+            val replacedText = replacements.fold(text) { acc, (from, to) ->
+                acc.replace(from, to)
+            }
+
+            file.writeText(replacedText)
+        }
 }
