438f89bb53a6fa7cbc8c1983f0692d9b9a2bf58e
Filip Dolník <github@filipdolnik.com>
2023-07-28 20:01:06 +0200

    Temporarily disable analytics.

--- SKIE/common/configuration/declaration/src/commonMain/kotlin/co/touchlab/skie/configuration/AnalyticsTier.kt
+++ SKIE/common/configuration/declaration/src/commonMain/kotlin/co/touchlab/skie/configuration/AnalyticsTier.kt
@@ -3,15 +3,7 @@ package co.touchlab.skie.configuration
 enum class AnalyticsTier(val features: Set<SkieFeature>) {
 
     All(
-        SkieFeature.Analytics_Compiler,
-        SkieFeature.Analytics_Gradle,
-        SkieFeature.Analytics_Hardware,
-        SkieFeature.Analytics_GradlePerformance,
-        SkieFeature.Analytics_SkiePerformance,
-        SkieFeature.Analytics_SkieConfiguration,
-        SkieFeature.Analytics_Sysctl,
-        SkieFeature.Analytics_OpenSource,
-        SkieFeature.Analytics_GitRemotes,
+        // WIP
     ),
 
     NoTracking(
