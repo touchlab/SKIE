3deb62e05d040e5ecfece13bd74f080f0cdea47a
Filip Dolník <github@filipdolnik.com>
2023-08-03 16:34:51 +0200

    Implement replaceData task.

--- SKIE/build.gradle.kts
+++ SKIE/build.gradle.kts
@@ -1,7 +1,3 @@
-import co.touchlab.skie.buildsetup.plugins.DevRoot
-import io.github.gradlenexus.publishplugin.NexusPublishExtension
-import io.github.gradlenexus.publishplugin.NexusPublishPlugin
-
 plugins {
     id("skie.root")
     alias(libs.plugins.gradleDoctor)
--- build-setup/src/main/kotlin/co/touchlab/skie/buildsetup/plugins/SkieRoot.kt
+++ build-setup/src/main/kotlin/co/touchlab/skie/buildsetup/plugins/SkieRoot.kt
@@ -3,10 +3,53 @@ package co.touchlab.skie.buildsetup.plugins
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.apply
+import java.io.File
 
-abstract class SkieRoot: Plugin<Project> {
+abstract class SkieRoot : Plugin<Project> {
+
     override fun apply(project: Project): Unit = with(project) {
         apply<DevRoot>()
         apply<SkieBase>()
+
+        registerReplaceDataTask()
+    }
+
+    private fun Project.registerReplaceDataTask() {
+        tasks.register("replaceData") {
+            doLast {
+                val replacementString = System.getenv()["SKIE_REPLACEMENT_STRING"] ?: return@doLast
+
+                replaceData(replacementString)
+            }
+        }
     }
 }
+
+private fun Project.replaceData(replacementString: String) {
+    val replacements = parseReplacementString(replacementString)
+
+    projectDir.walkTopDown()
+        .filter { it.isFile && it.extension in listOf("kt", "kts", "properties", "json") }
+        .forEach {
+            it.replace(replacements)
+        }
+}
+
+private fun File.replace(replacements: List<Replacement>) {
+    val fileContent = readText()
+
+    val newContent = replacements.fold(fileContent) { acc, replacement ->
+        acc.replace(replacement.from, replacement.to)
+    }
+
+    if (newContent != fileContent) {
+        writeText(newContent)
+    }
+}
+
+private fun parseReplacementString(replacementString: String): List<Replacement> =
+    replacementString
+        .split("&&&")
+        .map { Replacement(it.substringBefore("|||"), it.substringAfter("|||")) }
+
+private data class Replacement(val from: String, val to: String)
