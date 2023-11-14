615c1f5d8c2cffb0ce1989b48fca0a7c6be4900c
Filip Dolník <github@filipdolnik.com>
2023-11-14 16:56:33 +0100

    Fix fake inherited objc constructors not being generated for non-public constructors.

--- SKIE/compiler/kotlin-plugin/src/kgp_common/kotlin/co/touchlab/skie/phases/oir/CreateFakeObjCConstructorsPhase.kt
+++ SKIE/compiler/kotlin-plugin/src/kgp_common/kotlin/co/touchlab/skie/phases/oir/CreateFakeObjCConstructorsPhase.kt
@@ -2,7 +2,6 @@ package co.touchlab.skie.phases.oir
 
 import co.touchlab.skie.oir.element.OirClass
 import co.touchlab.skie.oir.element.OirConstructor
-import co.touchlab.skie.oir.element.OirVisibility
 import co.touchlab.skie.oir.element.constructors
 import co.touchlab.skie.oir.element.copyValueParametersFrom
 import co.touchlab.skie.oir.element.superClass
@@ -25,9 +24,7 @@ object CreateFakeObjCConstructorsPhase : SirPhase {
 
         val constructorObjCSelectors = this.constructors.map { it.selector }.toSet()
 
-        val parentConstructors = superClass.constructors.filter { it.visibility == OirVisibility.Public }
-
-        val missingConstructors = parentConstructors.filter { it.selector !in constructorObjCSelectors }
+        val missingConstructors = superClass.constructors.filter { it.selector !in constructorObjCSelectors }
 
         missingConstructors.forEach {
             addFakeObjCConstructor(it)
@@ -46,7 +43,10 @@ object CreateFakeObjCConstructorsPhase : SirPhase {
 
         oirConstructor.originalSirConstructor = parentConstructor.originalSirConstructor.shallowCopy(
             parent = this.originalSirClass,
-            visibility = SirVisibility.Private,
+            visibility = when (parentConstructor.originalSirConstructor.visibility) {
+                SirVisibility.Removed -> SirVisibility.Removed
+                else -> SirVisibility.Private
+            },
         ).apply {
             copyValueParametersFrom(parentConstructor.originalSirConstructor)
         }
