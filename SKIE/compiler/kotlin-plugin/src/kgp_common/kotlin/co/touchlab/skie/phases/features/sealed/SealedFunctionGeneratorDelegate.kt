b25e832578e5226fd5fe2569582b2e983d3ad331
Filip Dolník <github@filipdolnik.com>
2024-03-06 13:12:50 +0100

    Fix an overload collision of onEnum(of:) caused by its optional overload.

--- SKIE/compiler/kotlin-plugin/src/kgp_common/kotlin/co/touchlab/skie/phases/features/sealed/SealedFunctionGeneratorDelegate.kt
+++ SKIE/compiler/kotlin-plugin/src/kgp_common/kotlin/co/touchlab/skie/phases/features/sealed/SealedFunctionGeneratorDelegate.kt
@@ -26,6 +26,7 @@ class SealedFunctionGeneratorDelegate(
     fun generate(kirClass: KirClass, enum: SirClass) {
         val requiredFunction = generateRequiredOverload(kirClass, enum)
         generateOptionalOverload(kirClass, enum, requiredFunction)
+        generateRequiredOverloadWithOptionalReturnType(kirClass, enum, requiredFunction)
     }
 
     private fun generateRequiredOverload(kirClass: KirClass, enum: SirClass): SirSimpleFunction =
@@ -41,6 +42,16 @@ class SealedFunctionGeneratorDelegate(
             returnTypeModifier = { it.toNullable() },
         ).apply {
             addOptionalFunctionBody(requiredFunction)
+        }
+    }
+
+    private fun generateRequiredOverloadWithOptionalReturnType(kirClass: KirClass, enum: SirClass, requiredFunction: SirSimpleFunction) {
+        createFunctionDeclaration(
+            kirClass = kirClass,
+            enum = enum,
+            returnTypeModifier = { it.toNullable() },
+        ).apply {
+            addRequiredOverloadWithOptionalReturnTypeFunctionBody(requiredFunction)
         }
     }
 
@@ -171,6 +182,18 @@ class SealedFunctionGeneratorDelegate(
                     .nextControlFlow("else")
                     .add("return nil")
                     .endControlFlow("else")
+                    .build(),
+            )
+        }
+    }
+
+    private fun SirSimpleFunction.addRequiredOverloadWithOptionalReturnTypeFunctionBody(requiredFunction: SirSimpleFunction) {
+        bodyBuilder.add {
+            val valueParameter = valueParameters.first()
+
+            addCode(
+                CodeBlock.builder()
+                    .add("return %L as %T", requiredFunction.call(valueParameter), requiredFunction.returnType.evaluate().swiftPoetTypeName)
                     .build(),
             )
         }
