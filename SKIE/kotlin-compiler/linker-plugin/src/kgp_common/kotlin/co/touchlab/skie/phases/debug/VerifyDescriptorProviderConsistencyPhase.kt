2b7457528ec730e20ab052cfd9c0cf741e1b0073
Filip Dolník <github@filipdolnik.com>
2024-04-26 13:05:59 +0200

    Fix a false crash due to an expected difference in exposed descriptor algorithm.

--- SKIE/kotlin-compiler/linker-plugin/src/kgp_common/kotlin/co/touchlab/skie/phases/debug/VerifyDescriptorProviderConsistencyPhase.kt
+++ SKIE/kotlin-compiler/linker-plugin/src/kgp_common/kotlin/co/touchlab/skie/phases/debug/VerifyDescriptorProviderConsistencyPhase.kt
@@ -5,7 +5,9 @@ package co.touchlab.skie.phases.debug
 import co.touchlab.skie.configuration.SkieConfigurationFlag
 import co.touchlab.skie.phases.KirPhase
 import co.touchlab.skie.phases.descriptorProvider
+import co.touchlab.skie.phases.mapper
 import co.touchlab.skie.phases.objCExportedInterfaceProvider
+import org.jetbrains.kotlin.descriptors.ClassDescriptor
 import org.jetbrains.kotlin.resolve.descriptorUtil.fqNameUnsafe
 
 object VerifyDescriptorProviderConsistencyPhase : KirPhase {
@@ -18,7 +20,7 @@ object VerifyDescriptorProviderConsistencyPhase : KirPhase {
         val objCExportedInterface = objCExportedInterfaceProvider.objCExportedInterface
 
         val errors = listOfNotNull(
-            descriptorProvider.exposedClasses.shouldMatch(objCExportedInterface.generatedClasses) { fqNameUnsafe.asString() },
+            descriptorProvider.exposedClasses.shouldMatchExposed(objCExportedInterface.generatedClasses) { fqNameUnsafe.asString() },
             descriptorProvider.exposedCategoryMembers.shouldMatch(objCExportedInterface.categoryMembers.values.flatten().toSet()) { fqNameUnsafe.asString() },
             descriptorProvider.exposedTopLevelMembers.shouldMatch(objCExportedInterface.topLevel.values.flatten().toSet()) { fqNameUnsafe.asString() },
             descriptorProvider.exposedFiles.shouldMatch(objCExportedInterface.topLevel.keys) { name ?: "<Unknown file>" },
@@ -30,6 +32,15 @@ object VerifyDescriptorProviderConsistencyPhase : KirPhase {
                     errors.joinToString("\n") { "    $it" },
             )
         }
+    }
+
+    context(KirPhase.Context)
+    private fun Set<ClassDescriptor>.shouldMatchExposed(other: Set<ClassDescriptor>, fqName: ClassDescriptor.() -> String): String? {
+        // The Kotlin compiler (incorrectly) includes non-exported classes in certain cases - for example, "internal" exceptions from Java.
+        // SKIE intentionally excludes these classes, so the filter is needed before comparing the sets.
+        val exposedOther = other.filter { mapper.shouldBeExposed(it) }.toSet()
+
+        return this.shouldMatch(exposedOther, fqName)
     }
 
     private fun <T> Set<T>.shouldMatch(other: Set<T>, fqName: T.() -> String): String? {
