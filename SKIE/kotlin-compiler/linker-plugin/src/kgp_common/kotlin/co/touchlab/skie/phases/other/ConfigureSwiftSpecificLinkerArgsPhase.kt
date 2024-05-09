250b246bdc3ddb876f64eab321d2aeaa443ba16d
Filip Dolník <github@filipdolnik.com>
2024-05-09 16:33:22 +0200

    Fix dynamic frameworks in Kotlin 2.0.

--- SKIE/kotlin-compiler/linker-plugin/src/kgp_common/kotlin/co/touchlab/skie/phases/other/ConfigureSwiftSpecificLinkerArgsPhase.kt
+++ SKIE/kotlin-compiler/linker-plugin/src/kgp_common/kotlin/co/touchlab/skie/phases/other/ConfigureSwiftSpecificLinkerArgsPhase.kt
@@ -3,6 +3,8 @@ package co.touchlab.skie.phases.other
 import co.touchlab.skie.phases.LinkPhase
 import co.touchlab.skie.phases.configurables
 import co.touchlab.skie.phases.konanConfig
+import co.touchlab.skie.util.KotlinCompilerVersion
+import co.touchlab.skie.util.current
 import org.jetbrains.kotlin.backend.konan.KonanConfigKeys
 import org.jetbrains.kotlin.konan.target.platformName
 import java.io.File
@@ -12,7 +14,14 @@ object ConfigureSwiftSpecificLinkerArgsPhase : LinkPhase {
     context(LinkPhase.Context)
     override suspend fun execute() {
         val swiftLibSearchPaths = listOf(
-            File(configurables.absoluteTargetToolchain, "usr/lib/swift/${configurables.platformName().lowercase()}"),
+            File(
+                configurables.absoluteTargetToolchain,
+                if (KotlinCompilerVersion.current >= KotlinCompilerVersion.`2_0_0`) {
+                    "lib/swift/${configurables.platformName().lowercase()}"
+                } else {
+                    "usr/lib/swift/${configurables.platformName().lowercase()}"
+                },
+            ),
             File(configurables.absoluteTargetSysRoot, "usr/lib/swift"),
         ).flatMap { listOf("-L", it.absolutePath) }
 
