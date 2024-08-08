91a5d487b25ee7d87d425b837a098395dc42a2fe
Tadeas Kriz <tadeas@touchlab.co>
2024-08-08 11:17:05 -0400

    Update publish action.

--- SKIE/publish.sh
+++ SKIE/publish.sh
@@ -1,9 +1,7 @@
 set -xe
 
-export RELEASE_VERSION=0.9.0
-
 ./gradlew :initializeSonatypeStagingRepository
 
 ./gradlew findSonatypeStagingRepository -x initializeSonatypeStagingRepository publishToSonatype
 
-./gradlew :findSonatypeStagingRepository -x initializeSonatypeStagingRepository :closeSonatypeStagingRepository
+./gradlew :findSonatypeStagingRepository -x initializeSonatypeStagingRepository :closeAndReleaseSonatypeStagingRepository
