fe07884a6742824e3e6a25de4e1b4523edef1131
Tadeas Kriz <tadeas@touchlab.co>
2025-03-31 14:17:12 -0400

    Add support for Kotlin 2.1.20.

---
+++ SKIE/kotlin-compiler/linker-plugin/src/kgp_2.1.20/kotlin/co/touchlab/skie/compilerinject/interceptor/SimpleNamedPhaseInterceptorConfigurer.kgp_2.1.20.kt
@@ -0,0 +1,79 @@
+package co.touchlab.skie.compilerinject.interceptor
+
+import co.touchlab.skie.compilerinject.reflection.Reflector
+import org.jetbrains.kotlin.backend.konan.ConfigChecks
+import org.jetbrains.kotlin.config.CompilerConfiguration
+import org.jetbrains.kotlin.config.CompilerConfigurationKey
+import org.jetbrains.kotlin.config.LoggingContext
+import org.jetbrains.kotlin.config.phaser.SimpleNamedCompilerPhase
+import kotlin.reflect.jvm.jvmName
+
+class SimpleNamedPhaseInterceptorConfigurer<Context, Input, Output> :
+    PhaseInterceptorConfigurer<SimpleNamedCompilerPhase<Context, Input, Output>, Context, Input, Output>
+    where Context : LoggingContext, Context : ConfigChecks {
+
+    override fun canConfigurePhase(phase: Any): Boolean = phase is SimpleNamedCompilerPhase<*, *, *>
+
+    override fun configure(
+        configuration: CompilerConfiguration,
+        phase: SimpleNamedCompilerPhase<Context, Input, Output>,
+        interceptors: List<PhaseInterceptor<Context, Input, Output>>,
+    ) {
+        val namedPhase = phase.reflector
+        val chain = ErasedPhaseInterceptorChain(interceptors)
+        synchronized(phase) {
+            val currentPhaseBody = namedPhase.op
+            val (originalPhaseBody, interceptorKey) = if (currentPhaseBody.isIntercepted()) {
+                val interceptedPhaseBody = InterceptedPhaseBodyReflector(currentPhaseBody)
+                interceptedPhaseBody.originalPhaseBody to interceptedPhaseBody.interceptorKey
+            } else {
+                currentPhaseBody to CompilerConfigurationKey.create("phaseInterceptor for phase $phase")
+            }
+
+            configuration.put(interceptorKey, chain)
+
+            val interceptedPhaseBody = InterceptedPhaseBody(
+                originalPhaseBody,
+                interceptorKey,
+            )
+            namedPhase.op = interceptedPhaseBody
+        }
+    }
+
+    private val SimpleNamedCompilerPhase<Context, Input, Output>.reflector: SimpleNamedCompilerPhaseReflector<Context, Input, Output>
+        get() = SimpleNamedCompilerPhaseReflector(this)
+}
+
+private class InterceptedPhaseBody<Context, Input, Output>(
+    val originalPhaseBody: OriginalPhaseBody<Context, Input, Output>,
+    val interceptorKey: CompilerConfigurationKey<ErasedPhaseInterceptor<Context, Input, Output>>,
+) : (Context, Input) -> Output where Context : LoggingContext, Context : ConfigChecks {
+
+    override fun invoke(context: Context, input: Input): Output {
+        val interceptor = context.config.configuration.get(interceptorKey)
+        return if (interceptor != null) {
+            interceptor.invoke(context, input, originalPhaseBody)
+        } else {
+            originalPhaseBody.invoke(context, input)
+        }
+    }
+}
+
+private class SimpleNamedCompilerPhaseReflector<Context, Input, Output>(
+    override val instance: SimpleNamedCompilerPhase<Context, Input, Output>,
+) : Reflector(instance::class) where Context : LoggingContext, Context : ConfigChecks {
+
+    var op: (Context, Input) -> Output by declaredField("\$op")
+}
+
+private class InterceptedPhaseBodyReflector<Context, Input, Output>(
+    override val instance: (Context, Input) -> Output,
+) : Reflector(instance::class) where Context : LoggingContext, Context : ConfigChecks {
+
+    val originalPhaseBody: (Context, Input) -> Output by declaredField()
+    val interceptorKey: CompilerConfigurationKey<ErasedPhaseInterceptor<Context, Input, Output>> by declaredField()
+}
+
+private fun <Context, Input, Output> ((Context, Input) -> Output).isIntercepted(): Boolean where Context : LoggingContext, Context : ConfigChecks {
+    return javaClass.name == InterceptedPhaseBody::class.jvmName
+}
---
+++ SKIE/kotlin-compiler/linker-plugin/src/kgp_2.1.20/kotlin/co/touchlab/skie/kir/irbuilder/util/DeserializedClassSourceFile.kgp_2.1.20.kt
@@ -0,0 +1,20 @@
+package co.touchlab.skie.kir.irbuilder.util
+
+import co.touchlab.skie.shim.findPackage
+import org.jetbrains.kotlin.descriptors.DeclarationDescriptor
+import org.jetbrains.kotlin.descriptors.SourceFile
+import org.jetbrains.kotlin.library.metadata.DeserializedSourceFile
+import org.jetbrains.kotlin.library.metadata.KlibMetadataDeserializedPackageFragment
+import org.jetbrains.kotlin.library.metadata.KlibMetadataProtoBuf
+import org.jetbrains.kotlin.library.metadata.kotlinLibrary
+import org.jetbrains.kotlin.resolve.descriptorUtil.module
+import org.jetbrains.kotlin.serialization.deserialization.descriptors.DeserializedClassDescriptor
+
+actual fun DeserializedClassDescriptor.findSourceFile(): SourceFile =
+    sourceByIndex(this, classProto.getExtension(KlibMetadataProtoBuf.classFile))
+
+private fun sourceByIndex(descriptor: DeclarationDescriptor, index: Int): SourceFile {
+    val fragment = descriptor.findPackage() as KlibMetadataDeserializedPackageFragment
+    val fileName = fragment.proto.strings.stringList[index]
+    return DeserializedSourceFile(fileName, descriptor.module.kotlinLibrary)
+}
