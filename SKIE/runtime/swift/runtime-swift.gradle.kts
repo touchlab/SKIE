3a7e9fec47b341ea510261b6563e3af901927698
Filip Dolník <github@filipdolnik.com>
2025-10-22 15:45:04 +0200

    Convert swift runtime module to the new build setup.

--- SKIE/runtime/swift/runtime-swift.gradle.kts
+++ SKIE/runtime/swift/runtime-swift.gradle.kts
@@ -8,35 +8,3 @@ skiePublishing {
     description = "Swift part of the SKIE runtime. It's used to facilitate certain features of SKIE."
     publishSources = true
 }
-
-// WIP Move to plugin
-val createResourcesIndex by tasks.registering {
-    val resourcesProvider = sourceSets.main.map { it.resources.files }
-
-    val outputFileProvider = sourceSets.main.map {
-        it.output.resourcesDir!!.resolve("co/touchlab/skie/runtime/index.txt")
-    }
-
-    inputs.files(resourcesProvider)
-    outputs.file(outputFileProvider)
-
-    doLast {
-        val indexContent = resourcesProvider.get().joinToString("\n") { sourceSets.main.get().resourceName(it) }
-
-        val outputFile = outputFileProvider.get()
-        outputFile.parentFile.mkdirs()
-        outputFile.writeText("$indexContent\n")
-    }
-}
-
-tasks.named("processResources") {
-    dependsOn(createResourcesIndex)
-}
-
-fun SourceSet.resourceName(file: File): String {
-    val baseResourcesPaths = this.resources.srcDirs
-
-    val baseResourcesFolder = baseResourcesPaths.first { file.startsWith(it) }
-
-    return file.relativeTo(baseResourcesFolder).path
-}
--- build-setup/src/main/kotlin/co/touchlab/skie/buildsetup/main/plugins/skie/SkieRuntimeSwift.kt
+++ build-setup/src/main/kotlin/co/touchlab/skie/buildsetup/main/plugins/skie/SkieRuntimeSwift.kt
@@ -3,13 +3,61 @@ package co.touchlab.skie.buildsetup.main.plugins.skie
 import co.touchlab.skie.buildsetup.main.plugins.base.BaseKotlin
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.Task
+import org.gradle.api.tasks.SourceSet
+import org.gradle.api.tasks.SourceSetContainer
+import org.gradle.api.tasks.TaskProvider
 import org.gradle.kotlin.dsl.apply
 import org.jetbrains.kotlin.gradle.plugin.KotlinPluginWrapper
+import java.io.File
 
 abstract class SkieRuntimeSwift : Plugin<Project> {
 
     override fun apply(target: Project): Unit = with(target) {
         apply<BaseKotlin>()
         apply<KotlinPluginWrapper>()
+
+        configureCreateResourcesIndexTask()
+    }
+
+    private fun Project.configureCreateResourcesIndexTask() {
+        val createResourcesIndex = registerCreateResourcesIndexTask()
+
+        tasks.named("processResources").configure {
+            dependsOn(createResourcesIndex)
+        }
+    }
+
+    private fun Project.registerCreateResourcesIndexTask(): TaskProvider<Task> {
+        val sourceSets = extensions.getByName("sourceSets") as SourceSetContainer
+
+        return tasks.register("createResourcesIndex") {
+            val mainSourceSet = sourceSets.named("main")
+
+            val resourcesProvider = mainSourceSet.map { it.resources.files }
+
+            val outputFileProvider = mainSourceSet.map {
+                it.output.resourcesDir!!.resolve("co/touchlab/skie/runtime/index.txt")
+            }
+
+            inputs.files(resourcesProvider)
+            outputs.file(outputFileProvider)
+
+            doLast {
+                val indexContent = resourcesProvider.get().joinToString("\n") { mainSourceSet.get().resourceName(it) }
+
+                val outputFile = outputFileProvider.get()
+                outputFile.parentFile.mkdirs()
+                outputFile.writeText("$indexContent\n")
+            }
+        }
+    }
+
+    private fun SourceSet.resourceName(file: File): String {
+        val baseResourcesPaths = this.resources.srcDirs
+
+        val baseResourcesFolder = baseResourcesPaths.first { file.startsWith(it) }
+
+        return file.relativeTo(baseResourcesFolder).path
     }
 }
