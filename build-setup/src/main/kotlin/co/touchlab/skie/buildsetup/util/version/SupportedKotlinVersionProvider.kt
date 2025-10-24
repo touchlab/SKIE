5a1a7bc0973c7774c488c0434f3870da8ed01c77
Filip Dolník <github@filipdolnik.com>
2025-10-24 13:18:55 +0200

    Allow changing compiler version from the enabled versions property.

--- build-setup/src/main/kotlin/co/touchlab/skie/buildsetup/util/version/SupportedKotlinVersionProvider.kt
+++ build-setup/src/main/kotlin/co/touchlab/skie/buildsetup/util/version/SupportedKotlinVersionProvider.kt
@@ -8,27 +8,33 @@ object SupportedKotlinVersionProvider {
         val enabledKotlinVersions = getEnabledKotlinVersions(project)
 
         val activeSupportedVersion = enabledKotlinVersions.maxByOrNull { it.name }
-            ?: error("Primary Kotlin version cannot be determined as no supported versions are enabled.")
+            ?: error("Primary Kotlin version cannot be determined as no supported versions are enabled. " +
+                "Check for mistakes in the `versionSupport.kotlin.enabledVersions` property.")
 
         return activeSupportedVersion
     }
 
     fun getEnabledKotlinVersions(project: Project): List<SupportedKotlinVersion> {
-        val enabledVersions = project.findProperty("versionSupport.kotlin.enabledVersions")
+        val enabledVersionNames = getParsedEnabledVersionNames(project)
+
+        val supportedKotlinVersions = getSupportedKotlinVersions(project)
+
+        return if (enabledVersionNames.isNotEmpty()) {
+            supportedKotlinVersions.filter { supportedVersion ->
+                supportedVersion.name.toString() in enabledVersionNames || supportedVersion.compilerVersion.toString() in enabledVersionNames
+            }
+        } else {
+            supportedKotlinVersions
+        }
+    }
+
+    private fun getParsedEnabledVersionNames(project: Project): List<String> =
+        project.findProperty("versionSupport.kotlin.enabledVersions")
             ?.toString()
             ?.split(",")
             ?.map { it.trim() }
             ?.filter { it.isNotBlank() }
             ?: emptyList()
-
-        val supportedKotlinVersions = getSupportedKotlinVersions(project)
-
-        return if (enabledVersions.isNotEmpty()) {
-            supportedKotlinVersions.filter { it.name.toString() in enabledVersions }
-        } else {
-            supportedKotlinVersions
-        }
-    }
 
     fun getMinimumSupportedKotlinVersion(project: Project): KotlinToolingVersion =
         getSupportedKotlinVersions(project).flatMap { it.supportedVersions }.min()
@@ -45,13 +51,17 @@ object SupportedKotlinVersionProvider {
      * Otherwise, we would have to rename everything once the full version is released.
      *
      * The () parentheses can be used to specify other supported versions (the compiler version is supported by default).
+     *
+     * Note: The compiler version can be alternatively set by setting `versionSupport.kotlin.enabledVersions` to one of the other supported versions.
      */
     fun getSupportedKotlinVersions(project: Project): List<SupportedKotlinVersion> {
         val supportedKotlinVersions = project.property("versionSupport.kotlin").toString()
 
         val versions = splitVersions(supportedKotlinVersions)
 
-        return versions.map { parseVersion(it) }
+        val enabledVersionNames = getParsedEnabledVersionNames(project)
+
+        return versions.map { parseVersion(it, enabledVersionNames) }
     }
 
     private fun splitVersions(supportedKotlinVersions: String): List<String> {
@@ -88,7 +98,7 @@ object SupportedKotlinVersionProvider {
 
     private val kotlinVersionRegex = "([^\\[(\\])]+)(?:\\[([^\\[(\\])]+)])?(?:\\(([^\\[(\\])]+)\\))?".toRegex()
 
-    private fun parseVersion(version: String): SupportedKotlinVersion {
+    private fun parseVersion(version: String, enabledVersionNames: List<String>): SupportedKotlinVersion {
         val trimmedVersion = version.trim()
 
         val match = kotlinVersionRegex.matchEntire(trimmedVersion) ?: error("Invalid Kotlin version identifier: $trimmedVersion")
@@ -96,13 +106,15 @@ object SupportedKotlinVersionProvider {
         val name = match.groups[1]?.value?.let(::KotlinToolingVersion)
             ?: error("Invalid Kotlin version identifier - missing name: $trimmedVersion")
 
-        val compilerVersion = match.groups[2]?.value?.let(::KotlinToolingVersion) ?: name
-
-        val otherSupportedVersions = match.groups[3]?.value
+        val otherSupportedVersionNames = match.groups[3]?.value
             ?.split(",")
             ?.map { it.trim() }
-            ?.map(::KotlinToolingVersion)
             ?: emptyList()
+
+        val otherSupportedVersions = otherSupportedVersionNames.map(::KotlinToolingVersion)
+
+        val compilerVersion = match.groups[2]?.value?.let(::KotlinToolingVersion)
+            ?: getCompilerVersion(name, otherSupportedVersionNames, enabledVersionNames)
 
         return SupportedKotlinVersion(
             name = name,
@@ -110,4 +122,15 @@ object SupportedKotlinVersionProvider {
             otherSupportedVersions = otherSupportedVersions,
         )
     }
+
+    private fun getCompilerVersion(
+        name: KotlinToolingVersion,
+        otherSupportedVersionNames: List<String>,
+        enabledVersionNames: List<String>,
+    ): KotlinToolingVersion =
+        if (name.toString() in enabledVersionNames) {
+            name
+        } else {
+            enabledVersionNames.find { it in otherSupportedVersionNames }?.let(::KotlinToolingVersion) ?: name
+        }
 }
