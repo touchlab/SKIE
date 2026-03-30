1a302e35699597e4cb00d5061e9372a4194539cb
Tadeas Kriz <tadeas@touchlab.co>
2026-03-30 01:10:29 -0400

    Fix acceptance tests.

--- SKIE/acceptance-tests/functional/src/test/resources/tests/bugs/generic_function_inherited_from_two_interfaces_is_duplicated_by_compiler/can_be_called.swift
+++ SKIE/acceptance-tests/functional/src/test/resources/tests/bugs/generic_function_inherited_from_two_interfaces_is_duplicated_by_compiler/can_be_called.swift
@@ -7,7 +7,7 @@ let i2: I2 = a
 var result = b.get(value: 6).int32Value
 result -= a.get(value: 3).int32Value
 
-result -= (i1.get(value: 2) as! KotlinInt).int32Value
-result -= (i2.get(value: 1) as! KotlinInt).int32Value
+result -= (i1.get(value: 2 as KotlinInt) as! KotlinInt).int32Value
+result -= (i2.get(value: 1 as KotlinInt) as! KotlinInt).int32Value
 
 exit(result)
