9e7c7f1d8ffea884fb1d606962f2e37f6b56729a
Carlo Marinangeli <carlo.marinangeli@careem.com>
2026-04-28 13:12:53 +0400

    Update dev-support and enabledVersions comment for Kotlin 2.3.21.
    - Bump dev-support KMP plugin version to 2.3.21.
    - Update commented enabledVersions helper to reflect 2.3.21.

--- dev-support/build.gradle.kts
+++ dev-support/build.gradle.kts
@@ -4,7 +4,8 @@ plugins {
 //     kotlin("multiplatform") version "2.2.20" apply false
 //     kotlin("multiplatform") version "2.2.21" apply false
 //     kotlin("multiplatform") version "2.3.0-RC" apply false
-    kotlin("multiplatform") version "2.3.20" apply false
+//     kotlin("multiplatform") version "2.3.20" apply false
+    kotlin("multiplatform") version "2.3.21" apply false
 }
 
 buildscript {
